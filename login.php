62da6e79d5fa3855736dceb62cc70c6aea63914b
Nirajan Shakya <nirajanshakya746@gmail.com>
2017-08-23 15:13:37 +0545

    Adding files to master branch

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+</title> </title>
+<head>
+
+</head>
+<body>
+<link rel='stylesheet' href='style.css'>
+<form>
+<div class = "Login">
+<input type="text" placeholder="Username" name="username" required>
+<input type="password" placeholder="Password" name ="password" required>
+<input type = "submit" value="Log In">
+</div>
+</form>
+</body>
+</html>
