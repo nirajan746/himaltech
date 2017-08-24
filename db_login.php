55fbaf617fa50a31791c70dac37b33abccc8a560
Nirajan Shakya <nirajanshakya746@gmail.com>
2017-08-24 12:10:45 +0545

    Login page finished

--- db_login.php
+++ db_login.php
@@ -4,24 +4,24 @@
 
 $conn = mysqli_connect('localhost','root','');
 $dbname='login_members';
-if (!conn){
-die ('No connection with the database');
-}
-echo 'connection successful!!';
+	if (!$conn){
+	die ('No connection with the database');
+	}
+	echo 'connection successful!!';
 
 //connecting database login_members
 
-mysqli_select_db($conn,$dbname) or die;
+	mysqli_select_db($conn,$dbname) or die;
 
 //retrieving data from the database
-$username=$_POST['Username'];
-$password=$_POST['Password'];
+	$username=$_POST['username'];
+	$password=$_POST['password'];
 
 //Query the database for user 
-$result= mysqli_query($conn,"select * from admin where Username='$username' and Password ='$password'") 
-or die('Error');
+	$result= mysqli_query($conn,"select * from admin where Username='$username' and Password ='$password'") 
+		or die('Error');
 
-$row=mysqli_fetch_array($resutl);
+$row=mysqli_fetch_array($result);
 
 if (!empty ($row['Username'])==$username && ($row['Password'])==$password){
 echo 'Login Successful'.'<br>'.'Admin:'.$row['Username'];
