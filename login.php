79fc62386ca880acff0400d11034cfb1b81e41d6
Nirajan Shakya <nirajanshakya746@gmail.com>
2017-08-24 10:11:28 +0545

    login page updated

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
 <body>
 <link rel='stylesheet' href='style.css'>
 <form>
-<div class = "Login">
+<div id='Login'>
 <input type="text" placeholder="Username" name="username" required>
 <input type="password" placeholder="Password" name ="password" required>
 <input type = "submit" value="Log In">
